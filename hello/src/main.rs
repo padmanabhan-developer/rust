de33a6975cf38e715cbb920a327fdc7e619cd9bf
Padmanabhan <padmanabhan.developer.email@gmail.com>
2022-08-06 07:33:45 +0530

    hello world app

---
+++ hello/src/main.rs
@@ -0,0 +1,16 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    one();
+}
+
+fn one() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
+}
